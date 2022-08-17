da010459ce3ab0a074797d639226a1c628539452
Frank Mckenna <frankmckenna@Franks-MBP.broadband>
2022-08-17 16:55:24 +0100

    feat: setup home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,60 @@
+import { View, Text, SafeAreaView, Platform, StyleSheet, StatusBar, Image, TextInput } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from "react-native-heroicons/outline";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
+  return (
+    <SafeAreaView style={styles.droidSafeArea} className="bg-white pt-5">
+      <Text >
+        <View className='flex-row pb-3 items-center mx-4 space-x-2 px-4'>
+          <Image source ={{
+            uri: "https://links.papareact.com/wru"
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1">
+            <Text className="font-bold text-grat-400 text-xs" >
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00ccbb" />
+              
+            </Text>
+          </View>
+          <UserIcon size={35} color="#00ccbb" />
+        </View>
+
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <SearchIcon color="gray" size={20} />
+            <TextInput
+              placeholder="Restaurants and cuisines"
+              keyboardType='default'
+            />
+          </View>
+          <AdjustmentsIcon color="#00ccbb" />
+        </View>
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  droidSafeArea: {
+      flex: 1,
+      backgroundColor: 'white',
+      paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+});
