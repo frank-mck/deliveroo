27d0519e7531aea4236ed7ecac8af518fe942871
Frank Mckenna <frankmckenna@Franks-MBP.broadband>
2022-08-18 14:54:29 +0100

    feat: add restaurantCard

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,14 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import RestaurantCard from './RestaurantCard'
 
 const FeaturedRow = ({ id, title, description }) => {
+  const restaurantCards = [
+    <RestaurantCard
+      id={'987'}
+      imgUrl='https://links.papareact.com/gn7'
+      title='Yo! Sushi'
+      rating={4.5}
+      genre="Japenese"
+      address="123  Main St"
+      short_description="Test description"
+      dishes={20}
+      long={20}
+      lat
+      key={2205}
+    />,
+  ]
+
+  const renderCards = ({ item }) => {
+    return item;
+  }
+
   return (
     <View>
       <View className="mt-4 flex-row items-center justify-between px-4">
         <Text className='font-bold text-lg'>{title}</Text>
           <ArrowRightIcon color='#00CCBB' />
       </View>
+      <Text className='text-xs text-gray-500 px-4'>
+        {description}
+      </Text>
+
+      <FlatList 
+        data={restaurantCards}
+        renderItem={renderCards}
+        keyExtractor={({ key }) => key}
+        horizontal
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        showsHorizontalScrollIndicator={false}
+        className='pt-4'
+      />
     </View>
   )
 }
--- components/categories.js
+++ components/categories.js
@@ -4,12 +4,12 @@ import CategoryCard from './CategoryCard'
 
 const Categories = () => {
   const categories = [
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title' />,
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title' />,
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title' />,
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title' />,
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title' />,
-    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title' />,
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title1' key={331} />,
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title2' key={222} />,
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title3' key={311} />,
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title4' key={4122} />,
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title5' key={1422} />,
+    <CategoryCard imgUrl='https://links.papareact.com/gn7' title='title6' key={1225} />,
   ]
 
   const renderItems = ({ item }) => {
@@ -21,6 +21,7 @@ const Categories = () => {
       data={categories}
       renderItem={renderItems}
       horizontal
+      keyExtractor={({ key }) => key}
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{
         paddingHorizontal: 15,
