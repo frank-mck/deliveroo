ba032b3c720946dff9430264e4456e347c150d53
Frank Mckenna <frankmckenna@Franks-MacBook-Pro.local>
2022-09-13 14:23:50 +0100

    feat: restaurantScreen icons

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -2,7 +2,7 @@ import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity';
-import { ArrowLeftIcon, StarIcon } from 'react-native-heroicons/outline';
+import { ArrowLeftIcon, ChevronRightIcon, LocationMarkerIcon, QuestionMarkCircleIcon, StarIcon } from 'react-native-heroicons/outline';
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
@@ -45,10 +45,29 @@ const RestaurantScreen = () => {
         <View className='px-4 pt-4'>
           <Text className='text-3xl font-bold'>{title}</Text>
           <View className='flex-row space-x-2 my-1'>
-            <View>
+            <View className='flex-row items-center space-x-1'>
               <StarIcon color='green' opacity={0.5} size={22} />
+              <Text className='text-xs text-gray-500'>
+                <Text className='text-green-500'>{rating}</Text> . {genre}
+              </Text>
+            </View>
+            <View className='flex-row items-center space-x-1'>
+              <LocationMarkerIcon color='gray' opacity={0.4} size={22} />
+              <Text className='text-gray-500'>Nearby . {address}</Text>
             </View>
           </View>
+          <Text className='text-gray-500 mt-2 pb-4'>{short_description}</Text>
+        </View>
+        <TouchableOpacity className='flex-row items-center space-x-2 p-4 border-y border-gray-300'>
+          <QuestionMarkCircleIcon color='gray' opacity={0.6} size={20} />
+          <Text className='pl-2 flex-1 text-md font-bold'>
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB" />
+        </TouchableOpacity>
+
+        <View>
+          
         </View>
       </View>
     </ScrollView>
