838bc751a48cf8e59ff6f8a2a73acafeeff358c6
Frank Mckenna <77457834+frank-mck@users.noreply.github.com>
2023-08-22 13:02:19 +0100

    Update sanity.js

--- sanity.js
+++ sanity.js
@@ -2,7 +2,7 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 const client = sanityClient({
-  projectId: "4dkhpsh8",
+  projectId: ENV.projectId,
   dataset: "production",
   useCdn: true,
   apiVersion: "2021-10-21"
